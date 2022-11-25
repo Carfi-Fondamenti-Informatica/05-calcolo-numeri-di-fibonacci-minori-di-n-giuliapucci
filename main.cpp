df7622765c8bb7317bbb2df491e651d68c2b2479
giuliapucci <115046420+giuliapucci@users.noreply.github.com>
2022-11-25 16:04:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   int n1 = 1;
+    int n2 = 1;
+    int n;
+    cin >> n;
+    int i=3;
+    if (n >= 2) {
+        cout << n1 << endl;
+        cout << n2 << endl;
+        while (i++ <= n) {
+            cout << n1 + n2 << endl;
+            if (n1 < n2)
+                n1 += n2;
+            else n2 += n1;
+        }
+    }else if (n == 1){
+        cout << "1" << endl;
+
+    }
    return 0;
 }
