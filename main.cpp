0d50a7473aac1ee0668961f834ce0f8421286b8a
giuliapucci <115046420+giuliapucci@users.noreply.github.com>
2022-11-25 21:58:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@ int a=1;
 int b=0;
 int c=0;
 int n=0;
-int i;
 cin >> n;
- if (n>2) {
+ if (n>0) {
      cout << a << endl;
-     for (i=0;i <= n;i++) {
+     while (c<n) {
          c=a+b;
          b=a;
          a=c;
@@ -18,11 +17,6 @@ cin >> n;
              cout << c << endl;
          }
      }
- } else if (n==1) {
-     cout << a << endl;
- } else if (n==2) {
-     cout << "1" << endl;
-     cout << "1" << endl;
  }
    return 0;
 }
